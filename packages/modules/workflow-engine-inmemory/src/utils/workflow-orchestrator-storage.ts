b7f655a0263d9a4a6f78227f4118af896dac1008
adrien2p <adrien.deperetti@gmail.com>
2025-03-11 15:39:53 +0100

    fix missing import

--- packages/modules/workflow-engine-inmemory/src/utils/workflow-orchestrator-storage.ts
+++ packages/modules/workflow-engine-inmemory/src/utils/workflow-orchestrator-storage.ts
@@ -14,6 +14,7 @@ import {
   MedusaError,
   TransactionState,
   TransactionStepState,
+  isPresent,
 } from "@medusajs/framework/utils"
 import { WorkflowOrchestratorService } from "@services"
 import { CronExpression, parseExpression } from "cron-parser"
