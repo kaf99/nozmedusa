5d073e7795d7bd82124e4eba79ccb40f82d53a22
Riqwan Thamir <rmthamir@gmail.com>
2025-03-17 13:44:17 +0100

    chore: fix specs

--- integration-tests/http/__tests__/plugins/admin/medusa-config.js
+++
@@ -1,7 +0,0 @@
-const medusaConfig = require("../../../medusa-config.js")
-const path = require("path")
-
-module.exports = {
-  ...(medusaConfig ?? {}),
-  plugins: ["medusa-plugin-navigation"],
-}
