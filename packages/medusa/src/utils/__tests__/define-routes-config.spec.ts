6a83a665b421fa2ad629b2544a0e83e9ce39631a
Harminder Virk <virk.officials@gmail.com>
2025-04-15 19:22:45 +0530

    test: remove redundant test

--- packages/medusa/src/utils/__tests__/define-routes-config.spec.ts
+++ packages/medusa/src/utils/__tests__/define-routes-config.spec.ts
@@ -20,38 +20,4 @@ describe("defineMiddlewares", function () {
       ],
     })
   })
-
-  test("should wrap additionalDataValidator to middleware", () => {
-    const req = {
-      body: {},
-    } as MedusaRequest
-    const res = {} as MedusaResponse
-    const nextFn = jest.fn()
-    const schema = {
-      brand_id: zod.string(),
-    }
-
-    const config = defineMiddlewares([
-      {
-        matcher: "/admin/products",
-        additionalDataValidator: schema,
-      },
-    ])
-
-    expect(config).toMatchObject({
-      routes: [
-        {
-          matcher: "/admin/products",
-          middlewares: [expect.any(Function)],
-        },
-      ],
-    })
-
-    config.routes?.[0].middlewares?.[0](req, res, nextFn)
-    expect(req.additionalDataValidator!.parse({ brand_id: "1" })).toMatchObject(
-      {
-        brand_id: "1",
-      }
-    )
-  })
 })
