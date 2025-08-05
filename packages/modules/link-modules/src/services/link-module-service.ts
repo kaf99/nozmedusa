3387013576b7d5d978706c3aeddc2fe0d9b41458
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2025-08-05 06:30:55 -0300

    fix dismiss events and index custom modules

--- packages/modules/link-modules/src/services/link-module-service.ts
+++ packages/modules/link-modules/src/services/link-module-service.ts
@@ -224,6 +224,7 @@ export default class LinkModuleService implements ILinkModule {
   }
 
   @InjectTransactionManager()
+  @EmitEvents()
   async dismiss(
     primaryKeyOrBulkData: string | string[] | [string | string[], string][],
     foreignKeyData?: string,
@@ -243,7 +244,17 @@ export default class LinkModuleService implements ILinkModule {
       )
     }
 
-    const links = await this.linkService_.dismiss(data, sharedContext)
+    let links = await this.linkService_.dismiss(data, sharedContext)
+
+    moduleEventBuilderFactory({
+      action: CommonEvents.DETACHED,
+      object: this.entityName_,
+      source: this.serviceName_,
+      eventName: this.entityName_ + "." + CommonEvents.DETACHED,
+    })({
+      data: links.map((link) => link.id),
+      sharedContext,
+    })
 
     return (await this.baseRepository_.serialize(links)) as unknown[]
   }
