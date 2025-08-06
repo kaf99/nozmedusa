cc6de29fbf9d42ac5a8fc43e91d9856913518c7b
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2025-08-06 10:31:15 -0300

    link if FF on

--- integration-tests/modules/src/links/product-brand.ts
+++ integration-tests/modules/src/links/product-brand.ts
@@ -2,15 +2,20 @@ import ProductModule from "@medusajs/medusa/product"
 import { defineLink } from "@medusajs/utils"
 import BrandModule from "../modules/brand"
 
-export default defineLink(
-  {
-    linkable: ProductModule.linkable.product.id,
-    filterable: ["description", "material"],
-    isList: true,
-  },
-  {
-    linkable: BrandModule.linkable.brand.id,
-    filterable: ["id", "name"],
-    isList: false,
-  }
-)
+const link =
+  process.env.ENABLE_INDEX_MODULE === "true"
+    ? defineLink(
+        {
+          linkable: ProductModule.linkable.product.id,
+          filterable: ["description", "material"],
+          isList: true,
+        },
+        {
+          linkable: BrandModule.linkable.brand.id,
+          filterable: ["id", "name"],
+          isList: false,
+        }
+      )
+    : {}
+
+export default link
