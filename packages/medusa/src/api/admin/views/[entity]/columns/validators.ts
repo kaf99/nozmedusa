1b7a161198537ea0c21794aad96318be1c3bd1fc
Sebastian Rindom <skrindom@gmail.com>
2025-08-11 10:45:13 +0200

    fix: cleanup

--- packages/medusa/src/api/admin/views/[entity]/columns/validators.ts
+++ packages/medusa/src/api/admin/views/[entity]/columns/validators.ts
@@ -3,8 +3,3 @@ import { createSelectParams } from "../../../../utils/validators"
 
 export type AdminGetColumnsParamsType = z.infer<typeof AdminGetColumnsParams>
 export const AdminGetColumnsParams = createSelectParams()
-
-export type AdminUpdateColumnVisibilityType = z.infer<typeof AdminUpdateColumnVisibility>
-export const AdminUpdateColumnVisibility = z.object({
-  visible_columns: z.array(z.string()),
-})
