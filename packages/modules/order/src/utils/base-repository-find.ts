6b5aaf81adc9e833c5831bacc4f027c4ae5f0ea5
adrien2p <adrien.deperetti@gmail.com>
2025-09-03 18:51:23 +0200

    change order strategy to balanced

--- packages/modules/order/src/utils/base-repository-find.ts
+++ packages/modules/order/src/utils/base-repository-find.ts
@@ -20,7 +20,7 @@ export function setFindMethods<T>(klass: Constructor<T>, entity: any) {
     if (!("strategy" in findOptions_.options)) {
       if (findOptions_.options.limit != null || findOptions_.options.offset) {
         Object.assign(findOptions_.options, {
-          strategy: LoadStrategy.SELECT_IN,
+          strategy: LoadStrategy.BALANCED,
         })
       }
     }
