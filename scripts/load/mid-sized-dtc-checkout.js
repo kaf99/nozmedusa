669ee602a63ed588227193f995c341dd5bc3ebb5
olivermrbl <oliver@mrbltech.com>
2025-09-08 09:31:36 +0200

    work on more scenarios

--- scripts/load/mid-sized-dtc-checkout.js
+++ scripts/load/mid-sized-dtc-checkout.js
@@ -10,11 +10,11 @@ let publishableKey =
   "pk_937f7a595bd4b039bb6bbb95476dd036dd79187f31ef61cf7093f2b81a1f863b"
 let regionId = "reg_01K2ZDG12VKJ64F2NFTNW7Y8AT"
 let endpoint = "https://dtc-starter.medusajs.app"
-let projectId = 4182986
+let projectId = 4297226
 
 // let publishableKey =
-// "pk_50d624e919cfa298e85386577373ae4ebeb5c2d0a5a7588e1e7cb67536ae7f36"
-// let regionId = "reg_01K3RNJ7YR58VKM0WXJHHJBMQX"
+//   "pk_03823fccbc94952c4e2a6d045adb1b3479389ccacef6e4d5198c5e7b2a9dc4b5"
+// let regionId = "reg_01K4JKNGBA9HXBS57EJFSXY2X7"
 // let endpoint = "http://localhost:9000"
 
 const params = {
@@ -24,17 +24,10 @@ const params = {
   },
 }
 
-/*
- * k6 load test for mid-sized DTC ecommerce site (100 orders/day, ~3333 daily visitors, 3% conversion).
- * Simulates  load (~400 VUs: 320 browser, 60 shopper, 20 buyer) in 12 min (2m ramp-up, 8m steady, 2m ramp-down).
- * Tests: browsing, cart operations, checkout
- */
 export const options = {
   cloud: {
-    // Project: Load tests
     projectID: projectId,
-    // Test runs with the same name groups test runs together.
-    name: `Load test, mid-sized DTC, checkout ${new Date().toLocaleString()}`,
+    name: `Load test, simulating mid-sized DTC ${new Date().toLocaleString()}`,
   },
   scenarios: {
     browseCatalog: {
@@ -49,21 +42,57 @@ export const options = {
       gracefulRampDown: "30s",
       tags: { scenario: "browseCatalog" },
     },
-    addToCart: {
+    addBrowseAddAbandon: {
       executor: "ramping-vus",
-      exec: "addToCart",
+      exec: "addBrowseAddAbandon",
       startTime: "0s",
       stages: [
-        { duration: "2m", target: 60 },
-        { duration: "8m", target: 60 },
+        { duration: "2m", target: 15 },
+        { duration: "8m", target: 15 },
         { duration: "2m", target: 0 },
       ],
       gracefulRampDown: "30s",
-      tags: { scenario: "addToCart" },
+      tags: { scenario: "addBrowseAddAbandon" },
     },
-    completeCart: {
+    addBrowseAddComplete: {
       executor: "ramping-vus",
-      exec: "completeCart",
+      exec: "addBrowseAddComplete",
+      startTime: "0s",
+      stages: [
+        { duration: "2m", target: 10 },
+        { duration: "8m", target: 10 },
+        { duration: "2m", target: 0 },
+      ],
+      gracefulRampDown: "30s",
+      tags: { scenario: "addBrowseAddComplete" },
+    },
+    addMultipleAbandon: {
+      executor: "ramping-vus",
+      exec: "addMultipleAbandon",
+      startTime: "0s",
+      stages: [
+        { duration: "2m", target: 15 },
+        { duration: "8m", target: 15 },
+        { duration: "2m", target: 0 },
+      ],
+      gracefulRampDown: "30s",
+      tags: { scenario: "addMultipleAbandon" },
+    },
+    addMultipleComplete: {
+      executor: "ramping-vus",
+      exec: "addMultipleComplete",
+      startTime: "0s",
+      stages: [
+        { duration: "2m", target: 10 },
+        { duration: "8m", target: 10 },
+        { duration: "2m", target: 0 },
+      ],
+      gracefulRampDown: "30s",
+      tags: { scenario: "addMultipleComplete" },
+    },
+    randomShoppers: {
+      executor: "ramping-vus",
+      exec: "addToCart",
       startTime: "0s",
       stages: [
         { duration: "2m", target: 20 },
@@ -71,13 +100,16 @@ export const options = {
         { duration: "2m", target: 0 },
       ],
       gracefulRampDown: "30s",
-      tags: { scenario: "completeCart" },
+      tags: { scenario: "randomShoppers" },
     },
   },
   thresholds: {
     "http_req_duration{scenario:browseCatalog}": ["p(95)<400"],
-    "http_req_duration{scenario:addToCart}": ["p(95)<600"],
-    "http_req_duration{scenario:completeCart}": ["p(95)<1000"],
+    "http_req_duration{scenario:addBrowseAddAbandon}": ["p(95)<700"],
+    "http_req_duration{scenario:addBrowseAddComplete}": ["p(95)<1200"],
+    "http_req_duration{scenario:addMultipleAbandon}": ["p(95)<700"],
+    "http_req_duration{scenario:addMultipleComplete}": ["p(95)<1200"],
+    "http_req_duration{scenario:randomShoppers}": ["p(95)<1000"],
     http_req_failed: ["rate<0.01"],
   },
 }
@@ -87,7 +119,6 @@ http.setResponseCallback(http.expectedStatuses({ min: 200, max: 401 }))
 
 export function browseCatalog() {
   return group("Browse Flow", () => {
-    // Load homepage data (collections, categories, regions, cart, customer)
     const [regionsRes] = http.batch([
       { method: "GET", url: `${endpoint}/store/regions`, params },
       { method: "GET", url: `${endpoint}/store/collections`, params },
@@ -98,13 +129,11 @@ export function browseCatalog() {
     check(regionsRes, { "regions ok": (r) => r.status === 200 })
     sleep(2 + Math.random() * 3)
 
-    // Pick a random region on every run
     const regions = JSON.parse(regionsRes.body).regions
     regionId = regions[Math.floor(Math.random() * regions.length)].id
 
     const productsParams = `region_id=${regionId}&fields=*variants.calculated_price,+variants.inventory_quantity,+metadata,+tags`
 
-    // Browse collections and productions
     let res = http.get(`${endpoint}/store/collections`, params)
     check(res, { "collections ok": (r) => r.status === 200 })
     sleep(2 + Math.random() * 3)
@@ -123,192 +152,308 @@ export function browseCatalog() {
   })
 }
 
-export function addToCart() {
-  return group("Shop Flow", () => {
-    // Browse catalog
+function createCart() {
+  const res = http.post(
+    `${endpoint}/store/carts`,
+    JSON.stringify({
+      region_id: regionId,
+    }),
+    params
+  )
+  check(res, { "create cart ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+  return JSON.parse(res.body).cart
+}
+
+function addItemToCart(cartId, variantId, quantity) {
+  const res = http.post(
+    `${endpoint}/store/carts/${cartId}/line-items`,
+    JSON.stringify({
+      variant_id: variantId,
+      quantity: quantity,
+    }),
+    params
+  )
+  check(res, { "add to cart ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+  return JSON.parse(res.body).cart
+}
+
+function viewCart(cartId) {
+  const cartParams =
+    "fields=*items,*region,*items.product,*items.variant,*items.thumbnail,*items.metadata,+items.total,*promotions,+shipping_methods.name"
+  const res = http.get(
+    `${endpoint}/store/carts/${cartId}?${cartParams}`,
+    params
+  )
+  check(res, { "view cart ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+  return JSON.parse(res.body).cart
+}
+
+function applyPromotion(cartId) {
+  if (Math.random() < 0.1) {
+    const res = http.post(
+      `${endpoint}/store/carts/${cartId}/promotions`,
+      JSON.stringify({
+        promo_codes: ["10OFF"],
+      }),
+      params
+    )
+    check(res, { "add promotion ok": (r) => r.status === 200 })
+    sleep(1 + Math.random() * 2)
+    return JSON.parse(res.body).cart
+  }
+  return null
+}
+
+function browseMoreProducts() {
+  const productsParams = `region_id=${regionId}&fields=*variants.calculated_price,+variants.inventory_quantity,+metadata,+tags`
+  const res = http.get(`${endpoint}/store/products?${productsParams}`, params)
+  check(res, { "browse more products ok": (r) => r.status === 200 })
+  sleep(3 + Math.random() * 4)
+  return JSON.parse(res.body).products
+}
+
+function checkout(cart) {
+  const [res, paymentProvidersResp, shippingOptionsResp] = http.batch([
+    {
+      method: "GET",
+      url: `${endpoint}/store/carts/${cart.id}`,
+      params,
+    },
+    {
+      method: "GET",
+      url: `${endpoint}/store/payment-providers?region_id=${regionId}`,
+      params,
+    },
+    {
+      method: "GET",
+      url: `${endpoint}/store/shipping-options?cart_id=${cart.id}`,
+      params,
+    },
+  ])
+  check(res, { "view cart ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+
+  const selectedRegion = http.get(
+    `${endpoint}/store/regions/${regionId}`,
+    params
+  )
+  check(selectedRegion, { "selected region ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+
+  const country = JSON.parse(selectedRegion.body).region.countries[0].iso_2
+
+  http.post(
+    `${endpoint}/store/carts/${cart.id}`,
+    JSON.stringify({
+      shipping_address: {
+        first_name: "John",
+        last_name: "Doe",
+        address_1: "Some street.",
+        address_2: "Some alley",
+        company: "ACME",
+        postal_code: "13456",
+        city: "Wonderland",
+        country_code: country,
+        province: "QC",
+        phone: "1234567",
+      },
+      email: "john.doe@example.com",
+    }),
+    params
+  )
+  sleep(Math.random() * 3)
+
+  http.post(
+    `${endpoint}/store/carts/${cart.id}/shipping-methods`,
+    JSON.stringify({
+      option_id: JSON.parse(shippingOptionsResp.body).shipping_options[0].id,
+    }),
+    params
+  )
+  sleep(Math.random() * 3)
+
+  let paymentCollectionRes = http.post(
+    `${endpoint}/store/payment-collections`,
+    JSON.stringify({
+      cart_id: cart.id,
+    }),
+    params
+  )
+  check(paymentCollectionRes, {
+    "create payment collection ok": (r) => r.status === 200,
+  })
+  sleep(2 + Math.random() * 3)
+
+  const paymentCollection = JSON.parse(
+    paymentCollectionRes.body
+  ).payment_collection
+
+  let paymentSessionRes = http.post(
+    `${endpoint}/store/payment-collections/${paymentCollection.id}/payment-sessions`,
+    JSON.stringify({
+      provider_id: JSON.parse(paymentProvidersResp.body).payment_providers[0]
+        .id,
+    }),
+    params
+  )
+  check(paymentSessionRes, {
+    "create payment session ok": (r) => r.status === 200,
+  })
+  sleep(2 + Math.random() * 3)
+
+  let orderRes = http.post(
+    `${endpoint}/store/carts/${cart.id}/complete`,
+    JSON.stringify({}),
+    params
+  )
+  check(orderRes, { "create order ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+
+  const order = JSON.parse(orderRes.body).order
+  sleep(Math.random() * 3)
+
+  orderRes = http.get(`${endpoint}/store/orders/${order.id}`, params)
+  check(orderRes, { "view order ok": (r) => r.status === 200 })
+  sleep(2 + Math.random() * 3)
+
+  return order
+}
+
+export function addBrowseAddAbandon() {
+  return group("Shop Flow - Add Browse Add Abandon", () => {
     const products = browseCatalog()
+    const cart = createCart()
 
-    const cartParams =
-      "fields=*items,*region,*items.product,*items.variant,*items.thumbnail,*items.metadata,+items.total,*promotions,+shipping_methods.name"
-    // Create a cart
-    let res = http.post(
-      `${endpoint}/store/carts`,
-      JSON.stringify({
-        region_id: regionId,
-      }),
-      params
+    const firstProductIndex = Math.floor(Math.random() * products.length)
+    addItemToCart(
+      cart.id,
+      products[firstProductIndex].variants[0].id,
+      1 + Math.floor(Math.random() * 3),
+      "first item"
     )
-    check(res, { "create cart ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
 
-    const cart = JSON.parse(res.body).cart
+    const moreProducts = browseMoreProducts()
 
-    res = http.post(
-      `${endpoint}/store/carts/${cart.id}/line-items`,
-      JSON.stringify({
-        variant_id: products[3].variants[0].id,
-        quantity: 3,
-      }),
-      params
+    const secondProductIndex = Math.floor(Math.random() * moreProducts.length)
+    const updatedCart = addItemToCart(
+      cart.id,
+      moreProducts[secondProductIndex].variants[0].id,
+      1 + Math.floor(Math.random() * 2),
+      "second item"
     )
-    check(res, { "add to cart ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
 
-    let updatedCart = JSON.parse(res.body).cart
+    viewCart(updatedCart.id)
+    const finalCart = applyPromotion(updatedCart.id) || updatedCart
 
-    // Go to cart
-    res = http.get(
-      `${endpoint}/store/carts/${updatedCart.id}?${cartParams}`,
-      params
-    )
-    check(res, { "view cart ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
-
-    res = http.post(
-      `${endpoint}/store/carts/${updatedCart.id}/line-items/${updatedCart.items[0].id}`,
-      JSON.stringify({
-        quantity: 4,
-      }),
-      params
-    )
-    check(res, { "change cart quantity ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
-
-    updatedCart = JSON.parse(res.body).cart
-
-    // Add promotion to approximately 10% of carts
-    if (Math.random() < 0.1) {
-      res = http.post(
-        `${endpoint}/store/carts/${updatedCart.id}/promotions`,
-        JSON.stringify({
-          promo_codes: ["10OFF"],
-        }),
-        params
-      )
-      check(res, { "add promotion ok": (r) => r.status === 200 })
-      sleep(1 + Math.random() * 2)
-
-      // Update cart reference to include promotion
-      updatedCart = JSON.parse(res.body).cart
-    }
-
-    return updatedCart
+    return finalCart
   })
 }
 
-export function completeCart() {
-  return group("Checkout", () => {
-    const updatedCart = addToCart()
-    // Go to Checkout, load cart, payment options, delivery options
-    const [res, paymentProvidersResp, shippingOptionsResp] = http.batch([
-      {
-        method: "GET",
-        url: `${endpoint}/store/carts/${updatedCart.id}`,
-        params,
-      },
-      {
-        method: "GET",
-        url: `${endpoint}/store/payment-providers?region_id=${regionId}`,
-        params,
-      },
-      {
-        method: "GET",
-        url: `${endpoint}/store/shipping-options?cart_id=${updatedCart.id}`,
-        params,
-      },
-    ])
-    check(res, { "view cart ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
+export function addBrowseAddComplete() {
+  return group("Shop Flow - Add Browse Add Complete", () => {
+    const products = browseCatalog()
+    const cart = createCart()
 
-    // Ensure the country code is within the randomly selected region
-    // Go to cart
-    const selectedRegion = http.get(
-      `${endpoint}/store/regions/${regionId}`,
-      params
+    const firstProductIndex = Math.floor(Math.random() * products.length)
+    addItemToCart(
+      cart.id,
+      products[firstProductIndex].variants[0].id,
+      1 + Math.floor(Math.random() * 3),
+      "first item"
     )
-    check(selectedRegion, { "selected region ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
 
-    const country = JSON.parse(selectedRegion.body).region.countries[0].iso_2
+    const moreProducts = browseMoreProducts()
 
-    // Set shipping address
-    http.post(
-      `${endpoint}/store/carts/${updatedCart.id}`,
-      JSON.stringify({
-        shipping_address: {
-          first_name: "John",
-          last_name: "Doe",
-          address_1: "Some street.",
-          address_2: "Some alley",
-          company: "ACME",
-          postal_code: "13456",
-          city: "Wonderland",
-          country_code: country,
-          province: "QC",
-          phone: "1234567",
-        },
-        email: "john.doe@example.com",
-      }),
-      params
+    const secondProductIndex = Math.floor(Math.random() * moreProducts.length)
+    const updatedCart = addItemToCart(
+      cart.id,
+      moreProducts[secondProductIndex].variants[0].id,
+      1 + Math.floor(Math.random() * 2),
+      "second item"
     )
-    sleep(Math.random() * 3)
 
-    // Select delivery option
-    http.post(
-      `${endpoint}/store/carts/${updatedCart.id}/shipping-methods`,
-      JSON.stringify({
-        option_id: JSON.parse(shippingOptionsResp.body).shipping_options[0].id,
-      }),
-      params
-    )
-    sleep(Math.random() * 3)
+    viewCart(updatedCart.id)
+    const finalCart = applyPromotion(updatedCart.id) || updatedCart
 
-    // Select payment method, update cart
-    let paymentCollectionRes = http.post(
-      `${endpoint}/store/payment-collections`,
-      JSON.stringify({
-        cart_id: updatedCart.id,
-      }),
-      params
-    )
-    check(paymentCollectionRes, {
-      "create payment collection ok": (r) => r.status === 200,
-    })
-    sleep(2 + Math.random() * 3)
-
-    const paymentCollection = JSON.parse(
-      paymentCollectionRes.body
-    ).payment_collection
-
-    let paymentSessionRes = http.post(
-      `${endpoint}/store/payment-collections/${paymentCollection.id}/payment-sessions`,
-      JSON.stringify({
-        provider_id: JSON.parse(paymentProvidersResp.body).payment_providers[0]
-          .id,
-      }),
-      params
-    )
-    check(paymentSessionRes, {
-      "create payment session ok": (r) => r.status === 200,
-    })
-    sleep(2 + Math.random() * 3)
-
-    let orderRes = http.post(
-      `${endpoint}/store/carts/${updatedCart.id}/complete`,
-      JSON.stringify({}),
-      params
-    )
-    check(orderRes, { "create order ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
-
-    const order = JSON.parse(orderRes.body).order
-    sleep(Math.random() * 3)
-
-    // Order confirmation, load order
-    orderRes = http.get(`${endpoint}/store/orders/${order.id}`, params)
-    check(res, { "view order ok": (r) => r.status === 200 })
-    sleep(2 + Math.random() * 3)
-
-    return order
+    return checkout(finalCart)
   })
+}
+
+export function addMultipleAbandon() {
+  return group("Shop Flow - Add Multiple Abandon", () => {
+    const products = browseCatalog()
+    const cart = createCart()
+
+    const numItems = 3 + Math.floor(Math.random() * 3)
+    let updatedCart = cart
+
+    for (let i = 0; i < numItems; i++) {
+      const productIndex = Math.floor(Math.random() * products.length)
+      const quantity = 1 + Math.floor(Math.random() * 3)
+      updatedCart = addItemToCart(
+        cart.id,
+        products[productIndex].variants[0].id,
+        quantity,
+        `item ${i + 1}`
+      )
+    }
+
+    viewCart(updatedCart.id)
+    const finalCart = applyPromotion(updatedCart.id) || updatedCart
+
+    return finalCart
+  })
+}
+
+export function addMultipleComplete() {
+  return group("Shop Flow - Add Multiple Complete", () => {
+    const products = browseCatalog()
+    const cart = createCart()
+
+    const numItems = 3 + Math.floor(Math.random() * 3)
+    let updatedCart = cart
+
+    for (let i = 0; i < numItems; i++) {
+      const productIndex = Math.floor(Math.random() * products.length)
+      const quantity = 1 + Math.floor(Math.random() * 3)
+      updatedCart = addItemToCart(
+        cart.id,
+        products[productIndex].variants[0].id,
+        quantity,
+        `item ${i + 1}`
+      )
+    }
+
+    viewCart(updatedCart.id)
+    const finalCart = applyPromotion(updatedCart.id) || updatedCart
+
+    return checkout(finalCart)
+  })
+}
+
+export function addToCart() {
+  const scenario = Math.random()
+
+  if (scenario < 0.25) {
+    return addBrowseAddAbandon()
+  } else if (scenario < 0.5) {
+    return addBrowseAddComplete()
+  } else if (scenario < 0.75) {
+    return addMultipleAbandon()
+  } else {
+    return addMultipleComplete()
+  }
+}
+
+export function completeCart() {
+  const scenario = Math.random()
+
+  if (scenario < 0.5) {
+    return addBrowseAddComplete()
+  } else {
+    return addMultipleComplete()
+  }
 }
