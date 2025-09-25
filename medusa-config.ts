91bf548be8be0da0b9550183d0324f9db7d8618a
kaf99 <this.noz99@gmail.com>
2025-09-25 06:04:07 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,3 +1,5 @@
+
+
 module.exports = defineConfig({
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
@@ -9,3 +11,37 @@ module.exports = defineConfig({
     backendUrl: process.env.MEDUSA_BACKEND_URL,
   }
 })
+
+module.exports = defineConfig({
+  projectConfig: {
+    // other config options...
+   redisUrl : process.env.REDIS_URL
+  },
+})
+
+module.exports = defineConfig({
+  // other configurations....
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
+});
+
